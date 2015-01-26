20c1c41f704e740a2b67b705a65811da3737a67f
ahernandezgarcia <ahernandezgarcia@ebay.com>
2015-01-26 15:37:38 -0500

    added testUser

---
+++ PagerWeatherTests/testUser.m
@@ -0,0 +1,44 @@
+//
+//  testUser.m
+//  PagerWeather
+//
+//  Created by Hernandez Garcia, Ricardo(AWF) on 1/26/15.
+//  Copyright (c) 2015 ricardo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "User.h"
+
+@interface testUser : XCTestCase
+
+@property (nonatomic, strong) User *testUser;
+
+@end
+
+@implementation testUser
+
+- (void)setUp {
+    [super setUp];
+    self.testUser = [User sharedUser];
+}
+
+- (void)tearDown {
+    [super tearDown];
+    self.testUser = nil;
+}
+
+- (void)testUserNotNil
+{
+    XCTAssertNotNil(self.testUser);
+}
+
+- (void)testUpdateTemperature
+{
+    [self.testUser updateTemperatureOption:YES];
+    self.testUser = nil;
+    self.testUser = [User sharedUser];
+    XCTAssertTrue(self.testUser.isFahrenheitSelected);
+}
+
+@end
